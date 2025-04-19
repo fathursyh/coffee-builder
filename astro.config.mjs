cf55d222a9ac7e13379e291327b9d614e9f27a20
fathursyh <fathur.syah1@gmail.com>
2025-04-19 20:02:34 +0700

    update : add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,25 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
       appType: 'custom',
   },
+
   trailingSlash: 'ignore',
   integrations: [react()],
   security: {checkOrigin: true},
+
   redirects: {
     '/products': '/products/1'
+  },
+
+  adapter: netlify(),
+  experimental: {
+    session: true,
   }
 });
