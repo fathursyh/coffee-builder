b119da3d58853cc3c7cde28229d92d34edf6edcf
fathursyh <fathur.syah1@gmail.com>
2025-05-31 06:38:05 +0700

    update : edit alert, edit home hero height.

--- src/components/react/composables/CustomAlert.tsx
+++ src/components/react/composables/CustomAlert.tsx
@@ -12,11 +12,11 @@ export default function CustomAlert({status, text}: AlertProps) {
         setVisible(true);
         const timeout = setTimeout(() => {
             setVisible(false);
+            clearTimeout(timeout);
         }, 3000);
-        return clearTimeout(timeout)
     }, [])
     return (
-        <div role="alert" className={`alert ${status === 'success' ? 'alert-success' : 'alert-error'} alert-soft fixed z-50 ${visible ? 'top-0' : '-top-20'} w-full rounded-none justify-center py-6 transition-all duration-300`}>
+        <div role="alert" className={`alert ${status === 'success' ? 'alert-success' : 'alert-error'} alert-soft fixed z-50 ${visible ? 'top-0' : '-top-20'} w-full rounded-none justify-center py-6 transition-all duration-300`} onClick={() => setVisible(false)}>
             <span>{text}</span>
         </div>
     )
